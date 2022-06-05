5886cf8025543c153532622e055b3cce37241ff1
dglnyc <102766924+dglnyc@users.noreply.github.com>
2022-06-05 18:16:12 -0400

    another

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/api',(req, res)=>{
   res.json(data)
   console.log(res.json(data))
 })
-app.listen(process.env.PORT || PORT,"127.0.0.1",()=>{
+app.listen(process.env.PORT || PORT,()=>{
   console.log(`listening on ${PORT}`)
  
 })
