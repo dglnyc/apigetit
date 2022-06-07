098df697cde0183e1b0af776eab95541362a13ff
dglnyc <102766924+dglnyc@users.noreply.github.com>
2022-06-07 17:50:40 -0400

    now it's fancy with a link

--- server.js
+++ server.js
@@ -19,8 +19,9 @@ app.get('/api',(req, res)=>{
   res.json(data)
   console.log(res.json(data))
 })
-app.listen(process.env.PORT || PORT,()=>{
+app.listen(process.env.PORT || PORT,'127.0.0.1',()=>{
   console.log(`listening on ${PORT}`)
  
 })
 // server.listen(8000, "127.0.0.1")
+//server.listen(8000, '0.0.0.0')
